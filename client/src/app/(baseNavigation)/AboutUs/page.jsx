608bbfbaeb7ed968f36ed4c7c151eae2f356c1dc
Buddhika Madusanka <buddhikadevelopmetn@gmail.com>
2025-08-14 03:07:33 +0530

    do the changes in about us page

--- client/src/app/(baseNavigation)/AboutUs/page.jsx
+++ client/src/app/(baseNavigation)/AboutUs/page.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import Hero from "@/components/layouts/AboutUsPage/Hero";
-import AboutUsTitleSection from '@/components/layouts/AboutUsPage/AboutUsTitleSection';
+import AboutUsSection from '@/components/layouts/AboutUsPage/AboutUsSection';
 import AboutUsContent from '@/components/layouts/AboutUsPage/AboutUsContent';
 
 const page = () => {
   return (
     <div>
       <Hero />
-      <AboutUsTitleSection />
-      <AboutUsContent />
+
+      <div className='flex flex-col gap-8'>
+        <AboutUsSection />
+        <AboutUsContent />
+      </div>
     </div>
   )
 }
