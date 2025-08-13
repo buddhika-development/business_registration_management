e5b06e5db430452857f6f6afd58c9eada2332a88
Buddhika Madusanka <buddhikadevelopmetn@gmail.com>
2025-08-14 02:39:22 +0530

    setup the news letter section

--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Logo from "../components/ui/Logo";
 import Hero from "../components/layouts/HomePage/Hero";
+import Newsletter from "@/components/layouts/HomePage/Newsletter";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <Newsletter />
     </div>
   );
 }
