87c4612dd6691d5e922a94a55cacf745a9d3dff7
Viduni Umalya <dedimunigeviduni@gmail.com>
2025-08-13 14:05:31 +0530

    Error fix

--- client/src/components/ui/Title/AboutUsTitle.jsx
+++ client/src/components/ui/Title/AboutUsTitle.jsx
@@ -1,8 +1,8 @@
-import React, { Children } from 'react'
+// import React, { Children } from 'react'
 
 const AboutUsTitle = ({children, className}) => {
   return (
-    <h6 className={`leading-[1.2] ${className}`}>{children}</h6>
+    <h1 className={`leading-[1.2] ${className}`}>{children}</h1>
   )
 }
 
